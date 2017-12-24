ce4c57409961b5298f93710c39881b1122c325fa
Daniel Debonzi <debonzi@gmail.com>
2017-12-23 23:36:50 -0200

    Simple Profile working.

--- src/code/App.js
+++ src/code/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Navbar, Jumbotron, Button } from 'react-bootstrap';
+import { Grid, Navbar, Nav, NavItem, Jumbotron, Button } from 'react-bootstrap';
 
 
 class CharGrid extends Component {
@@ -9,13 +9,16 @@ class CharGrid extends Component {
   }
 
   componentDidMount() {
-    fetch('http://localhost:5000/api/chars', {credentials: 'include'})
+    let endpoint = process.env.REACT_APP_API_URI + '/api/chars'
+    fetch(endpoint, {credentials: 'include'})
     .then(results => {
       return results.json();
     }).then(data => {
       let chars = data.map((char) => {
         return(
-          <CharPortrait 
+          <CharPortrait
+              profile={this.props.profile}
+              uuid={char.uuid}  
               inset={char.inset_url}
               name={char.name}
               level={char.level}
@@ -27,7 +30,6 @@ class CharGrid extends Component {
         )
       })
     this.setState({chars: chars});
-    console.log("Chars", this.state.chars)
     })
   }
 
@@ -41,6 +43,67 @@ class CharGrid extends Component {
   }
 }
 
+
+class CharProfile extends Component {
+  constructor(props){
+    super(props);
+    this.state = {char: {}}
+  }
+
+  componentDidMount() {
+    let endpoint = process.env.REACT_APP_API_URI + '/api/chars/' + this.props.uuid
+    fetch(endpoint, {credentials: 'include'})
+    .then(results => {
+      return results.json();
+    }).then(data => {
+      this.setState({char: data});
+    })
+  }
+
+
+  render() {
+    return(
+    <div class="panel panel-default">
+      <div class="panel-body">
+        <div class="row">
+          <div class="col-xs-3 col-md-2">
+            <img class="side-bar-thumb" src={this.state.char.thumbnail_url}/>
+          </div>
+          <div class="col-xs-9 col-md-10">
+            <strong>
+              {this.state.char.name} ({this.state.char.level} {this.state.char.c_class}, <span>{this.state.char.realm})</span>
+            </strong>
+            <div class="row">
+              <div class="col-xs-6 col-md-6">
+                Current 2v2 Rating: <span class="main-char-stats-highlight"> {this.state.char.s2_cr} </span>
+              </div>
+              <div class="col-xs-6 col-md-6">
+                Current 3v3 Rating: <span class="main-char-stats-highlight"> {this.state.char.s2_cr} </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )}
+}
+
+
+class ProfileChooserHandler extends Component {
+  handleClick = () => {
+    console.log("UUID", this.props.uuid)
+    this.props.onCharClick(this.props.uuid);
+  }
+
+  render() {
+    return (
+      <h3 onClick={this.handleClick}>
+        {this.props.name}
+      </h3>
+    );
+  }
+}
+
 class CharPortrait extends Component {
   render()
   {
@@ -49,7 +112,7 @@ class CharPortrait extends Component {
         <div class="thumbnail">
           <img src={this.props.inset} />
           <div class="caption">
-            <h3>{this.props.name}</h3>
+            <ProfileChooserHandler onCharClick={this.props.profile} uuid={this.props.uuid} name={this.props.name} />
             <h5>({this.props.level} {this.props.class}, {this.props.realm})</h5>
             <ul class="main-char-stats">
               <li>Current 2v2 Rating: <span class="main-char-stats-highlight"> {this.props.s2_cr} </span></li>
@@ -63,33 +126,50 @@ class CharPortrait extends Component {
   }
 }
 
-class App extends Component {
 
-  // componentDidMount() {
-  //   console.log("DEbonzi");
-  //   fetch('http://localhost:5000/chars/api', {credentials: 'include'})
-  //   .then(results => {
-  //     console.log(results.json());
-  //   })
-  // }
+class ContentManager extends Component {
+  render() {
+    if (this.props.state.screen === 'char_grid'){
+      return <CharGrid  profile={this.props.profile} />
+    }
+    else if (this.props.state.screen === 'char_profile'){
+      return <CharProfile uuid={this.props.state.uuid} />
+    }
+  }
+}
+
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.showProfile = this.showProfile.bind(this)
+    this.state = {
+      screen: 'char_grid'
+    }
+  }
+
+  showProfile(uuid) {
+    this.setState({screen: 'char_profile', uuid: uuid});
+    console.log("showProfile", uuid)
+  }
 
   render() {
     return (
       <div>
-        <Navbar inverse fixedTop>
-          <Grid>
-            <Navbar.Header>
-              <Navbar.Brand>
-                <a href="/">React App</a>
-              </Navbar.Brand>
-              <Navbar.Toggle />
-            </Navbar.Header>
-          </Grid>
+        <Navbar inverse collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand />
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav>
+              <NavItem eventKey={1} href="#" onClick={() => this.setState({screen: 'char_grid'})}>Chars</NavItem>
+            </Nav>
+          </Navbar.Collapse>          
         </Navbar>
         <Jumbotron>
           <Grid>
-            <h1>Welcome to React</h1>
-            <CharGrid />
+            <ContentManager state={this.state} profile={this.showProfile}/>
           </Grid>
         </Jumbotron>
       </div>
